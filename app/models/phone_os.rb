6ab187286a9413433cee5af3e71a4d6aacb237bf
Kyle <kdornblaser@gmail.com>
2015-04-15 10:45:00 -0400

    Iniitial setup.

--- app/models/phone_os.rb
+++ app/models/phone_os.rb
@@ -1,3 +1,4 @@
 class PhoneOs < ActiveRecord::Base
   has_many :supported_os
+  has_many :smartwatch, through: :supported_os
 end
--- app/models/smartwatch.rb
+++ app/models/smartwatch.rb
@@ -1,3 +1,4 @@
 class Smartwatch < ActiveRecord::Base
   has_many :supported_os
+  has_many :phone_os, through: :supported_os
 end
